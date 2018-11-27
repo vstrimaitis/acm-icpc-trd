d2fa2209b7f5035bec0af89f3642edddb904ab70
vstrimaitis <vstrimaitis@gmail.com>
2018-11-27 19:35:36 +0200

    Added Dijkstra, Gaussian elim. and Grundy numbers

---
+++ src/5.math/13.gaussian-elimination.cpp
@@ -0,0 +1,45 @@
+// The last column of a is the right-hand side of the system.
+// Returns 0, 1 or oo - the number of solutions.
+// If at least one solution is found, it will be in ans
+int gauss (vector < vector<ld> > a, vector<ld> & ans) {
+    int n = (int) a.size();
+    int m = (int) a[0].size() - 1;
+
+    vector<int> where (m, -1);
+    for (int col=0, row=0; col<m && row<n; ++col) {
+        int sel = row;
+        for (int i=row; i<n; ++i)
+            if (abs (a[i][col]) > abs (a[sel][col]))
+                sel = i;
+        if (abs (a[sel][col]) < eps)
+            continue;
+        for (int i=col; i<=m; ++i)
+            swap (a[sel][i], a[row][i]);
+        where[col] = row;
+
+        for (int i=0; i<n; ++i)
+            if (i != row) {
+                ld c = a[i][col] / a[row][col];
+                for (int j=col; j<=m; ++j)
+                    a[i][j] -= a[row][j] * c;
+            }
+        ++row;
+    }
+
+    ans.assign (m, 0);
+    for (int i=0; i<m; ++i)
+        if (where[i] != -1)
+            ans[i] = a[where[i]][m] / a[where[i]][i];
+    for (int i=0; i<n; ++i) {
+        ld sum = 0;
+        for (int j=0; j<m; ++j)
+            sum += ans[j] * a[i][j];
+        if (abs (sum - a[i][m]) > eps)
+            return 0;
+    }
+
+    for (int i=0; i<m; ++i)
+        if (where[i] == -1)
+            return oo;
+    return 1;
+}
